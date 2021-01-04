ead90c7363853d8ebfd1751c96a65557787caa47
sara04z <76944455+sara04z@users.noreply.github.com>
2021-01-04 14:40:02 +0100

    blumen seite da

--- pages/day/blume.js
+++ pages/day/blume.js
@@ -1,9 +1,13 @@
-return (
-  <Layout>
-    <PageHeading title="Pflanzen"></PageHeading>
-    <p>Hallo Welt</p>
-    <p>hallo</p>
-    <Blog />
-  </Layout>
-);
-1 comment on commit 28fa761
+import Layout from "../components/Layout";
+import PageHeading from "../components/PageHeading";
+import Blog from "../components/Blog";
+export default function IndexPage() {
+  return (
+    <Layout>
+      <PageHeading title="Pflanzen"></PageHeading>
+      <p>Hallo Welt</p>
+      <p>hallo</p>
+      <Blog />
+    </Layout>
+  );
+}
