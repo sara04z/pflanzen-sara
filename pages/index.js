28fa76130efee29d85d274fb33c4ec2099c248af
sara04z <76944455+sara04z@users.noreply.github.com>
2021-01-04 14:32:10 +0100

    seite blume weg

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function IndexPage() {
   return (
     <Layout>
       <PageHeading title="Pflanzen"></PageHeading>
-      <p>Hallo Welt</p>
+      <p>hallo</p>
       <Blog />
     </Layout>
   );
