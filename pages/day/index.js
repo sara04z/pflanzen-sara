b7298fb144ce81aad030f6210dc8c3aa610b943f
sara04z <76944455+sara04z@users.noreply.github.com>
2021-01-04 14:56:51 +0100

    neu

--- pages/day/index.js
+++ pages/day/index.js
@@ -1,3 +1,13 @@
-export default function DayPage() {
-  return <div>Hello Day</div>
+import Layout from "../components/Layout";
+import PageHeading from "../components/PageHeading";
+import Blog from "../components/Blog";
+export default function IndexPage() {
+  return (
+    <Layout>
+      <PageHeading title="Pflanzen"></PageHeading>
+      <p>Hallo Welt</p>
+      <p>hallo</p>
+      <Blog />
+    </Layout>
+  );
 }
