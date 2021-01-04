8ef7978d8a8d3abe11424d59f3ec3036435362f6
sara04z <76944455+sara04z@users.noreply.github.com>
2021-01-04 14:02:33 +0100

    kakteen seite wiedergergestellt

---
+++ pages/kakteen.js
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+import PageHeading from "../components/PageHeading";
+import Blog from "../components/Blog";
+export default function KakteenPage() {
+  return (
+    <Layout>
+      <PageHeading title="Kakteen"></PageHeading>
+      <span className="p-2 m-2 text-gray-600">Hallo Welt</span>
+      <img
+        style={{ width: "320px" }}
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fjooinn.com%2Fimages%2Fnew-york-17.jpg&f=1&nofb=1"
+      ></img>
+      <Blog />
+    </Layout>
+  );
+}
